f7dd2fe58be5c7343f82e3183615a0d8a85561ce
Joseph <jvg_0119@yahoo.com>
2015-10-01 09:25:26 -0700

    assignment-48-upload-images-progress

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,12 +3,16 @@ class Post < ActiveRecord::Base
     belongs_to :user
     belongs_to :topic
 
+    mount_uploader :image, ImageUploader
+    
     default_scope { order('created_at DESC')}
 
 	validates :title, length: { minimum: 5 }, presence: true
    	validates :body, length: { minimum: 20 }, presence: true
    	validates :topic, presence: true
    	validates :user, presence: true
+
+
 
 end
 
